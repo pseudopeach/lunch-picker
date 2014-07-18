4e6c7742b730882e6ad8602a15b422ecb45f18fd
Sheri Wilson <smgwilson@gmail.com>
2014-07-17 17:15:46 -0700

    Attempts to make ActionMailer work
    Worry about this shit later

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
 
 def invite_email(user)
   @user = user
+  @admin_email = 'smgwilson37@gmail.com' #TODO - fix temporary hard-coding
   @url  = 'http://example.com/login'
   mail(to: @user, subject: 'Vote in the lunch election')
 end
